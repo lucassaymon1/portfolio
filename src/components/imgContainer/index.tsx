7d1c5b0343d9e0d4dd27ba7171a81e3c8420c681
lucas <lucas327ks@gmail.com>
2023-10-02 21:20:27 -0300

    feat: update projects section Layout and Design

--- src/components/imgContainer/index.tsx
+++ src/components/imgContainer/index.tsx
@@ -1,20 +1,47 @@
 import Image from 'next/image'
 import { StaticImageData } from 'next/image'
+import Tag from '../Tag'
+import { PropsWithChildren } from 'react'
 
 type ImgContainerProps = {
 	src: string | StaticImageData
 	title: string
+	direction: string
+	description: string
 }
 
-function ImgContainer({ src, title }: ImgContainerProps) {
+function ImgContainer({
+	src,
+	title,
+	direction,
+	description
+}: ImgContainerProps) {
 	return (
-		<div className="flex flex-col items-center justify-center">
-			<Image src={src} alt="Projeto Food Explorer" width={640} />
-			<a href=""></a>
-			<h3 className="text-4xl mb-6">{title}</h3>
-			<p className="text-lg">
-				Projeto desenvolvido com ReactJS, NodeJS e Styled Components
-			</p>
+		<div
+			className={`
+				bg-gray-200 p-12 rounded-3xl
+				md:flex ${
+					direction === 'left' ? 'flex-row' : 'flex-row-reverse'
+				} justify-between gap-12`}
+		>
+			<Image src={src} alt="Projeto Food Explorer" width={740} />
+
+			<div>
+				<a href=""></a>
+				<h3 className="text-4xl mb-6">{title}</h3>
+				<p className="text-lg mb-12">{description}</p>
+				<div className="flex justify-center align-center flex-wrap gap-2">
+					<Tag title="tag" />
+					<Tag title="tag" />
+					<Tag title="tag" />
+					<Tag title="tag" />
+					<Tag title="tag" />
+					<Tag title="tag" />
+					<Tag title="tag" />
+					<Tag title="tag" />
+					<Tag title="tag" />
+				</div>
+			</div>
 		</div>
 	)
 }
