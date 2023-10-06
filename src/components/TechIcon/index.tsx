7ae2a16b054e9911318a12873efaac38bbfd5656
lucas <lucas327ks@gmail.com>
2023-10-05 21:15:11 -0300

    feat: update design of tech-icons and section

--- src/components/TechIcon/index.tsx
+++ src/components/TechIcon/index.tsx
@@ -2,21 +2,14 @@ import { Children } from 'react'
 
 export function TechSvg({ icon: Icon, title }: any) {
 	return (
-		<div className="p-4 border-2 border-slate-400 shadow-md">
-			<div className="w-24 flex flex-col items-center justify-center gap-2">
-				{Icon && <Icon size="56px" />}
-				<h3 className="text-lg font-medium text-green-500">{title}</h3>
-			</div>
-		</div>
-	)
-}
-
-export function TechImg({ src, title }: any) {
-	return (
-		<div className="p-4 border-2 border-slate-400">
-			<div className="flex flex-col items-center justify-center gap-2">
-				<img src={src} alt={`logo ${title}`} width="72px" />
-				<h3 className="text-lg font-medium text-green-500">{title}</h3>
+		<div
+			className="p-4 pt-6 border-2 border-slate-500 shadow-md 
+			rounded-tl-3xl rounded-br-3xl
+			transition-all hover:-translate-y-2"
+		>
+			<div className="w-36 flex flex-col items-center justify-center gap-2">
+				{Icon && <Icon size="56px" color="#23c55e" />}
+				<h3 className="text-md font-medium text-slate-300">{title}</h3>
 			</div>
 		</div>
 	)
